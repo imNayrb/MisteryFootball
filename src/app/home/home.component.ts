f99a2e796babd39934a81668554157fba826d8b2
imNayrb <bryan.uppolo@gmail.com>
2025-01-24 09:16:06 +0100

    fix vari

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { ProductCarouselComponent } from "../product-carousel/product-carousel.component";
 import { CommonModule } from '@angular/common';
+import { ProductDetailsComponent } from '../product-details/product-details.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductCarouselComponent, CommonModule, RouterModule],
+  imports: [ProductCarouselComponent, CommonModule, RouterModule, ProductDetailsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
